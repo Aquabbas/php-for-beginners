3bd09f3ded4c1cf560c23ffa049074ee5217ccee
Abbas Hayder <abbashayder@hotmail.com>
2024-01-29 21:56:56 +0100

    refactor: make a php router
    In this lesson we learned about Routers, Status Codes (404), and Code Organization.

--- demo/controllers/contact.php
+++ demo/controllers/contact.php
@@ -2,6 +2,4 @@
 
 $heading = 'Get in Touch';
 
-require("functions.php");
-
 require "views/contact.view.php";
--- demo/views/partials/banner.php
+++ demo/views/partials/banner.php
@@ -1,7 +1,10 @@
 <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">
-            <center><?= $heading ?></center>
+            <!-- <center><?= $heading ?></center> -->
+            <center>
+                <?= urlIs('/') || urlIs('/about') || urlIs('/contact') ? $heading : '404' ?>
+            </center>
         </h1>
     </div>
 </header>
