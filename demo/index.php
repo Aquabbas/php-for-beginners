867d2443b417c374b7fa702f7b02c0d1fe08b78c
Abbas Hayder <abbashayder@hotmail.com>
2024-02-03 09:35:06 +0100

    fix: prevent sql injection through url

--- demo/index.php
+++ demo/index.php
@@ -8,7 +8,15 @@ $config = require('config.php');
 
 $db = new Database($config['database']);
 
-$posts = $db->query("SELECT * FROM posts;")->fetchAll();
+$id = $_GET['id'];
+
+// Question Mark Method
+// $query = "SELECT * FROM posts WHERE id = ?";
+// $posts = $db->query($query, [$id])->fetch();
+
+// Key Value Parameter Method
+$query = "SELECT * FROM posts WHERE id = :id";
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
 
